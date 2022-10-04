be0f4a7faae74dd56b31ec7318de57245c8d684e
Gaheriet Pataky <gaheriet.pataky@gmail.com>
2022-10-04 15:48:48 +0200

    Ajout categorie formulaire unique

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -7,9 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[UniqueEntity(
+    "titre", message: "Il existe déja un article possédant ce titre"
+)]
 class Article
 {
     #[ORM\Id]
